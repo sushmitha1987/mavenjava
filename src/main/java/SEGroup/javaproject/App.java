1f296237cc8851fd90a4e496be49f46363211a6d
sushmitha <sushmithachary369@gmail.com>
2024-11-19 14:57:31 +0530

    first commit to maven java project

---
+++ src/main/java/SEGroup/javaproject/App.java
@@ -0,0 +1,13 @@
+package SEGroup.javaproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! \n22BD1A664P\n Sushmitha " );
+    }
+}
